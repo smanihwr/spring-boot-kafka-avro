ad98cb2232fa60bb034e152725edfbf2d26d554b
Manikandan Sundaramoorthy <smanihwr@gmail.com>
2020-01-03 16:32:56 -0600

    initial commit

---
+++ src/main/java/io/confluent/developer/KafkaAvroApplication.java
@@ -0,0 +1,12 @@
+package io.confluent.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAvroApplication.class, args);
+    }
+}
